01163f8f1b714a19310ecfb4e54f1fbc49090a82
maddekenn <madeleine.kennback@ub.uu.se>
2019-02-14 13:09:18 +0000

    CORA-796, tables and views for subject

---
+++ docker/data/subjectView.sql
@@ -0,0 +1,7 @@
+create view subject_name_sv as select subject_id, subject_name from subject_name where locale = 'sv';
+
+create view subject_name_en as select subject_id, subject_name from subject_name where locale = 'en';
+
+create view subject_name_complete as select sv.subject_id as id, sv.subject_name as name, en.subject_name as alternative_name from subject_name_sv sv left join subject_name_en en on sv.subject_id = en.subject_id;
+
+create view nationalsubject as select s.subject_id as id, s.subject_code, sn.name as default_name, sn.alternative_name from subject s left join  subject_name_complete sn on s.subject_id = sn.id where s.subject_type_id = 57;
--- docker/initdb/importDb.sh
+++ docker/initdb/importDb.sh
@@ -2,8 +2,8 @@
 set -e
 
 
-#TABLES="organisation organisation_name organisation_address organisation_contact organisation_parent organisation_predecessor organisation_predecessor_description organisation_type organisation_type_name"
-TABLES="select organisation organisationView"
+#TABLES="organisation organisation_name organisation_address organisation_contact organisation_parent organisation_predecessor organisation_predecessor_description organisation_type organisation_type_name subject subject_name subject_parent subject_predecessor subject_type subject_type_name"
+TABLES="select organisation organisationView subject"
 
 for TABLE in $TABLES
 do
