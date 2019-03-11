5f9d39acddc4589fb4ddac99d6d2f7c308c031f1
madde <madde@c3664965ac29>
2019-03-11 14:33:55 +0000

    Changed view for national subject

--- docker/data/subjectView.sql
+++ docker/data/subjectView.sql
@@ -4,4 +4,4 @@ create view subject_name_en as select subject_id, subject_name from subject_name
 
 create view subject_name_complete as select sv.subject_id as id, sv.subject_name as name, en.subject_name as alternative_name from subject_name_sv sv left join subject_name_en en on sv.subject_id = en.subject_id;
 
-create view nationalsubject as select s.subject_id as id, s.subject_code, sn.name as default_name, sn.alternative_name,   p.parent_subject_id as parentid from subject s left join  subject_name_complete sn on s.subject_id = sn.id left join subject_parent p on s.subject_id = p.subject_id where s.subject_type_id = 57;
+create view nationalsubject as select s.subject_id as subject_id, s.subject_code, sn.name as default_name, sn.alternative_name from subject s left join  subject_name_complete sn on s.subject_id = sn.id where s.subject_type_id = 57;
