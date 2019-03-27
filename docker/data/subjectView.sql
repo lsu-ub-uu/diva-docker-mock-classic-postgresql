2e7c59e3d0865172122108dd8876ad49d9a62366
madde <madde@fbe9e3b1cfd4>
2019-03-27 09:01:00 +0000

    Subjects from db

--- docker/data/subjectView.sql
+++ docker/data/subjectView.sql
@@ -5,3 +5,5 @@ create view subject_name_en as select subject_id, subject_name from subject_name
 create view subject_name_complete as select sv.subject_id as id, sv.subject_name as name, en.subject_name as alternative_name from subject_name_sv sv left join subject_name_en en on sv.subject_id = en.subject_id;
 
 create view nationalsubject as select s.subject_id as subject_id, s.subject_code, sn.name as default_name, sn.alternative_name from subject s left join  subject_name_complete sn on s.subject_id = sn.id where s.subject_type_id = 57;
+
+create view subject_parent_view as select CAST()parent_subject_id as varchar), CAST(subject_id as varchar) from subject_parent;
