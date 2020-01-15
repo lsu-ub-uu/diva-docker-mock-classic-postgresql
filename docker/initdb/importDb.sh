ce466807cbda4f80c96d9382a385e92af4a5cc10
maddekenn <madeleine.kennback@ub.uu.se>
2020-01-15 10:40:20 +0000

    Added users to database

--- docker/initdb/importDb.sh
+++ docker/initdb/importDb.sh
@@ -3,7 +3,7 @@ set -e
 
 
 #TABLES="organisation organisation_name organisation_address organisation_contact organisation_parent organisation_predecessor organisation_predecessor_description organisation_type organisation_type_name"
-TABLES="select organisation organisationView"
+TABLES="select organisation organisationView user"
 
 for TABLE in $TABLES
 do
