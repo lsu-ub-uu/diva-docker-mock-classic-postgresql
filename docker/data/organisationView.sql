ce43fc70e57fda181ddd1a2f3eea41ecc9e94ae1
perebartrolisimo <pere.bartroli@ub.uu.se>
2020-01-30 08:48:37 +0000

    Adding addressId in the organisationView

--- docker/data/organisationView.sql
+++ docker/data/organisationView.sql
@@ -1,5 +1,5 @@
 CREATE VIEW public.divaOrganisation as select CAST(o.organisation_id as varchar) as id, o.domain, o.organisation_name as defaultname, o.closed_date, o.organisation_code, o.orgnumber, o.organisation_homepage, o.not_eligible, ot.organisation_type_code as type_code, coalesce(alt.organisation_name,'') as alternative_name,
-a.city, a.street, a.postbox, a.postnumber, a.country_code from public.organisation o left join organisation_type ot on o.organisation_type_id = ot.organisation_type_id left join organisation_name alt 
+a.address_id, a.city, a.street, a.postbox, a.postnumber, a.country_code from public.organisation o left join organisation_type ot on o.organisation_type_id = ot.organisation_type_id left join organisation_name alt 
 on o.organisation_id = alt.organisation_id left join organisation_address a on o.address_id = a.address_id; 
 
 CREATE VIEW public.divaOrganisationPredecessor as select CAST(op.organisation_id as varchar) as organisation_id, CAST(op.organisation_predecessor_id as varchar) as predecessor_id, opd.organisation_predecessor_id as predecessorDescriptionId, opd.description from organisation_predecessor op 
