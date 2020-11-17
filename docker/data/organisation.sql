c11980c96b9ca79f9d648eb564a42c324bb67a2a
perebartrolisimo <pere.bartroli@ub.uu.se>
2020-11-17 14:35:31 +0000

    Small fix

--- docker/data/organisation.sql
+++ docker/data/organisation.sql
@@ -226,6 +226,7 @@ COPY public.organisation (organisation_id, last_updated, domain, closed_date, ol
 1454	2020-09-16 16:15:27.408	uu	\N	\N	\N	Vetenskapsområdet för humaniora och samhällsvetenskap	sv	\N	\N	\N	http://something.se/	\N	50	f	t	t	f	\N
 1455	2020-09-16 16:15:27.408	uu	\N	\N	\N	Teologiska fakulteten	sv	\N	\N	\N	http://somethingelse.se/	\N	50	f	t	t	f	\N
 1756	2015-03-12 10:48:53.898	uu	\N	\N	\N	Gamla Uppsala universitet	sv	\N	\N	\N	http://uu.se	\N	50	f	t	t	f	202100-2935
+1757	2020-03-12 10:48:53.898	uu	\N	\N	\N	Gamla KTH	sv	\N	\N	\N	http://gamla.kth.se	\N	50	f	t	t	f	202100-2439
 \.
 
 
@@ -309,6 +310,71 @@ COPY public.organisation_name (organisation_name_id, last_updated, locale, organ
 8152	2017-03-21 09:38:33.943	en	Root KTH	1749
 8153	2017-03-21 09:38:33.943	en	En fitnesse test organiation	1453
 \.
+CREATE OR REPLACE VIEW public.divaOrganisation 
+as select CAST(o.organisation_id as varchar) as id, o.domain, o.organisation_name as defaultname, 
+o.organisation_name_locale, o.closed_date, o.organisation_code, o.orgnumber, o.organisation_homepage, 
+o.not_eligible, o.show_in_portal, o.show_in_defence, o.top_level, 
+ot.organisation_type_code as type_code, alt.organisation_name_id, 
+coalesce(alt.organisation_name,'') as alternative_name,
+a.address_id, a.city, a.street, a.postbox, a.postnumber, a.country_code 
+from public.organisation o 
+left join organisation_type ot on o.organisation_type_id = ot.organisation_type_id 
+left join organisation_name alt on o.organisation_id = alt.organisation_id 
+left join organisation_address a on o.address_id = a.address_id; 
+
+CREATE OR REPLACE VIEW public.divaOrganisationPredecessor 
+as select op.organisation_id, op.organisation_predecessor_id, opd.organisation_predecessor_id 
+as predecessorDescriptionId, opd.description 
+from organisation_predecessor op 
+left join organisation_predecessor_description opd on op.organisation_id = opd.organisation_id 
+AND op.organisation_predecessor_id = opd.predecessor_id;
+
+CREATE OR REPLACE VIEW public.divaOrganisationParent 
+as select CAST(organisation_id as varchar), organisation_parent_id 
+from organisation_parent;
+
+
+CREATE OR REPLACE VIEW public.commonorganisationview 
+as select o.organisation_id as id, o.domain, o.organisation_name as defaultname, 
+o.organisation_name_locale, o.closed_date, o.organisation_code, o.orgnumber, o.organisation_homepage, 
+o.not_eligible, o.show_in_portal, o.show_in_defence, o.top_level, 
+ot.organisation_type_code as type_code, alt.organisation_name_id, 
+coalesce(alt.organisation_name,'') as alternative_name,
+a.address_id, a.city, a.street, a.postbox, a.postnumber, a.country_code 
+from public.organisation o 
+left join organisation_type ot on o.organisation_type_id = ot.organisation_type_id 
+left join organisation_name alt on o.organisation_id = alt.organisation_id 
+left join organisation_address a on o.address_id = a.address_id where o.organisation_type_id <> 49; 
+
+CREATE OR REPLACE VIEW public.rootorganisationview 
+as select o.organisation_id as id, o.domain, o.organisation_name as defaultname, 
+o.organisation_name_locale, o.closed_date, o.organisation_code, o.orgnumber, o.organisation_homepage, 
+o.not_eligible, o.show_in_portal, o.show_in_defence, o.top_level, 
+ot.organisation_type_code as type_code, alt.organisation_name_id, 
+coalesce(alt.organisation_name,'') as alternative_name,
+a.address_id, a.city, a.street, a.postbox, a.postnumber, a.country_code 
+from public.organisation o 
+left join organisation_type ot on o.organisation_type_id = ot.organisation_type_id 
+left join organisation_name alt on o.organisation_id = alt.organisation_id 
+left join organisation_address a on o.address_id = a.address_id where o.organisation_type_id = 49; 
+
+CREATE OR REPLACE VIEW public.organisationview 
+as select o.organisation_id as id, o.domain, o.organisation_name as defaultname, 
+o.organisation_name_locale, o.closed_date, o.organisation_code, o.orgnumber, o.organisation_homepage, 
+o.not_eligible, o.show_in_portal, o.show_in_defence, o.top_level, 
+ot.organisation_type_code as type_code, alt.organisation_name_id, 
+coalesce(alt.organisation_name,'') as alternative_name,
+a.address_id, a.city, a.street, a.postbox, a.postnumber, a.country_code 
+from public.organisation o 
+left join organisation_type ot on o.organisation_type_id = ot.organisation_type_id 
+left join organisation_name alt on o.organisation_id = alt.organisation_id 
+left join organisation_address a on o.address_id = a.address_id; 
+
+
+CREATE OR REPLACE VIEW public.organisationrelations
+as select distinct op.organisation_id, op.organisation_predecessor_id as relation from organisation_predecessor op
+union all
+select oparent.organisation_id, oparent.organisation_parent_id from organisation_parent oparent;
 
 
 --
